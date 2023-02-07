c56c68112c322d79797852e9c5fcbd0673851d25
DOVI Sanvee Cedric <cedricdovi6@gmail.com>
2023-02-07 15:52:10 -0800

    Partie 7: persitence en base de données

--- src/main/java/fr/lernejo/todo/TodoListController.java
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -2,23 +2,25 @@ package fr.lernejo.todo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
 @RestController
 class TodoListController {
-    private final ArrayList<Todo> List  = new ArrayList<>();
 
+    private TodoRepository todoRepository;
+    public TodoListController(TodoRepository todoRepository){
+       this.todoRepository = todoRepository;
+    }
     @PostMapping("api/todo")
     public void PostTodo(Todo todo)
     {
-         this.List.add(todo);
+         this.todoRepository.save(new TodoEntity(todo.message(),todo.author()));
     }
 
     @GetMapping("api/todo")
-    public ArrayList GetTodo(){
-        return this.List;
+    public Iterable<TodoEntity> GetTodo(){
+        return this.todoRepository.findAll();
     }
 }
